806f15ecdfdbc5d6d6d3b4bfcfa18031cc1ec437
Sanjiv Singh <sk.sanjiv@gmail.com>
2023-04-18 19:45:12 +0530

    removed redundant script

--- taxi-sim/taxi.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-python taxi.py \
-	-e a3eiore48b4qfq-ats.iot.us-east-1.amazonaws.com \
-	--r setup/.certs/AmazonRootCA1.pem \
-	-c setup/.certs/TAXI_1_device.pem \
-	-k setup/.certs/TAXI_1_private.key 
