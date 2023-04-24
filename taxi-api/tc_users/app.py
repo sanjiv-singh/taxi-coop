cd58e9d7731231fe5269f6781b27ac0fb1a78a4a
KishoreK <kalurikishore@gmail.com>
2023-04-24 23:01:22 +0530

    User registration
    User registration review points

--- taxi-api/tc_users/app.py
+++ taxi-api/tc_users/app.py
@@ -17,28 +17,84 @@ client = pymongo.MongoClient(endpoint, username=username, password=password,
     tls='true', tlsCAFile='rds-combined-ca-bundle.pem', retryWrites='false')
 db = client.taxidb
 print("Connected to Amazon DocumentDB")
-user_collection = db['users']
+user_collection = db['user']
 
-def lambda_handler(event, context):
+def handle_get(event):
+    print(event)
+    if event.get('resource') == '/user/{id}':
+        # Get single record by _id
+        id = event['pathParameters']['id']
+        print(id)
+        result = user_collection.find_one({'_id': ObjectId(id)})
+        print(result)
+        result['_id'] = str(result['_id'])
+        print(result)
+        return {
+            "statusCode": 200,
+            "body": json.dumps(result)
+        }
+
+    # Get all all records
+    cursor = user_collection.find()
+    rows = []
+    for row in cursor:
+        row['_id'] = str(row['_id'])
+        rows.append(row)
+    print(rows)
+
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(rows)
+    }
+
+def handle_post(event):
     print(event)
     print('user lamda handler is called.')
-
-
     body = json.loads(event["body"])
     first_name = body["first_name"]
     last_name = body["last_name"]
-    email = body["email"]
- 
- 
+    email = body["email"]  
     pprint.pprint(first_name)
     pprint.pprint(last_name) 
 
    #Insert data
-    result = user_collection.insert_one(body)
-    print(f"Inserted into taxi history: {result.inserted_id}")
-    
+    result = user_collection.insert_one(body)  
 
     return {
         "statusCode": 200,
-        "body": "User registration is successful."        
+        "body": json.dumps(result)      
     }
+    
+def handle_delete(event):
+    id = event['pathParameters']['id']
+    print(id)
+    result = user_collection.delete_one({'_id': ObjectId(id)})
+    print(result)
+    result['_id'] = str(result['_id'])
+    print(result)
+        
+    return {
+            "statusCode": 200,
+            "body": json.dumps(result)
+    }
+    
+def handle_default(event):
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "Method not implemented",
+        }),
+    }
+
+def lambda_handler(event, context):
+    method = event["httpMethod"]
+    if method == 'GET':
+        return handle_get(event)
+    elif method == 'POST':
+        return handle_post(event)
+    elif method == 'DELETE':
+        return handle_delete(event)
+    else:
+        return handle_default(event)
+
