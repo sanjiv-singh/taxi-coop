c7ec4fe80748901c3a39f427c8ed4055e2f697e5
Sanjiv Singh <sk.sanjiv@gmail.com>
2023-04-25 20:03:34 +0530

    reverting branch changes

--- taxi-iot/setup.sh
+++ taxi-iot/setup.sh
@@ -2,20 +2,6 @@
 
 # Description: This script creates the AWS IoT thing type, thing group, and policy for the taxi fleet.
 
-if [ -z "$DB_USER" ]; then
-	echo "DB_USER is not set"
-	echo "Please set the environment variable DB_USER"
-	echo "e.g. export DB_USER=taxi"
-	exit 1
-fi
-if [ -z "$DB_PASSWORD" ]; then
-	echo "DB_PASSWORD is not set"
-	echo "Please set the environment variable DB_PASSWORD"
-	echo "e.g. export DB_PASSWORD=taxi"
-	exit 1
-fi
-
-
 iot_registration_policy_arn='arn:aws:iam::aws:policy/service-role/AWSIoTThingsRegistration'
 iot_logging_policy_arn='arn:aws:iam::aws:policy/service-role/AWSIoTLogging'
 iot_rule_actions_policy_arn='arn:aws:iam::aws:policy/service-role/AWSIoTRuleActions'
@@ -27,69 +13,6 @@ fi
 if [ ! -f .certs/AmazonRootCA1.pem ]; then
     wget -O .certs/AmazonRootCA1.pem https://www.amazontrust.com/repository/AmazonRootCA1.pem
 fi
-
-
-create_lambda () {
-    lambda_execution_policy_arn="arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-    docdb_policy_arn="arn:aws:iam::aws:policy/AmazonDocDBFullAccess"
-    iot_policy_arn="arn:aws:iam::aws:policy/AWSIoTFullAccess"
-    aws iam create-role --role-name taxi-iot-lambda-role \
-        --assume-role-policy-document file://../taxi-db/lambda_policy.json --output text
-    aws iam attach-role-policy --role-name taxi-iot-lambda-role \
-        --policy-arn $lambda_execution_policy_arn
-    aws iam attach-role-policy --role-name taxi-iot-lambda-role \
-        --policy-arn $docdb_policy_arn
-    aws iam attach-role-policy --role-name taxi-iot-lambda-role \
-        --policy-arn $iot_policy_arn
-    aws iam put-role-policy \
-	    --role-name taxi-iot-lambda-role \
-	    --policy-name taxi-lambda-ec2 \
-	    --policy-document file://../taxi-db/lambda_ec2_policy.json
-    role_arn=$(aws iam get-role --role-name "taxi-iot-lambda-role" \
-        --query "Role.Arn" --output text)
-    sleep 5
-
-    security_group_ids=""
-    for security_group in $(aws ec2 describe-security-groups --query SecurityGroups[].GroupId --output text); do
-	if [ -z "$security_group_ids" ]; then
-		security_group_ids+=$security_group
-	else
-		security_group_ids+=","
-		security_group_ids+=$security_group
-	fi
-    done
-
-    subnet_ids=""
-    for subnet in $(aws ec2 describe-subnets --query Subnets[].SubnetId --output text); do
-	if [ -z "$subnet_ids" ]; then
-		subnet_ids+=$subnet
-	else
-		subnet_ids+=","
-		subnet_ids+=$subnet
-	fi
-    done
-
-    end_point=$(aws docdb describe-db-clusters --query DBClusters[].Endpoint --output text)
-
-
-    zip -r taxi_iot_lambda_package.zip taxi_iot_lambda.py
-    aws lambda create-function \
-        --function-name taxi_iot_lambda \
-        --runtime python3.9 \
-        --environment "Variables={db_user=$DB_USER,db_pass=$DB_PASSWORD,db_endpoint=$end_point}" \
-        --zip-file fileb://taxi_iot_lambda_package.zip \
-        --handler taxi_iot_lambda.lambda_handler \
-        --role $role_arn \
-        --vpc-config SubnetIds=$subnet_ids,SecurityGroupIds=$security_group_ids
-
-    aws lambda add-permission \
-	--function-name taxi_iot_lambda \
-	--statement-id iot-events \
-	--action "lambda:InvokeFunction" \
-	--principal api.amazonaws.com
-
-}
-
 
 echo "Creating IoT policy....."
 aws iot create-policy \
@@ -141,9 +64,6 @@ aws iot create-thing-group \
 	--thing-group-name Taxi_group \
 	--thing-group-properties "thingGroupDescription=Taxi Group"
 echo "done"
-
-echo "Creating lambda function for device registration"
-#create_lambda
 
 echo "Creating IoT topic rule....."
 echo "Run this step only after creating the lambda function taxidb_lambda"
