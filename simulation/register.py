9e5e967ccd3cbe5664c9ddb906a0a58d138a1e70
Sanjiv Singh <sk.sanjiv@gmail.com>
2023-04-27 16:36:05 +0530

    updated registration to use sample data

--- simulation/register.py
+++ simulation/register.py
@@ -2,6 +2,14 @@ import requests
 import json
 import asyncio
 import random
+import boto3
+
+AWS_API_GATEWAY = boto3.client('apigateway')
+
+
+def read_taxis(path: str):
+    with open(path, "r") as f:
+        return json.load(f)
 
 class RegistrationSimulator:
     "Manages registration of taxis through API"
@@ -12,7 +20,9 @@ class RegistrationSimulator:
     def __init__(self, end_point=""):
         self.end_point = end_point
         if not end_point:
-            self.end_point = "https://jm5y81zn02.execute-api.us-east-1.amazonaws.com/Prod/register_taxi/"
+            resp = AWS_API_GATEWAY.get_rest_apis()
+            api_id = resp.get("items")[0]["id"]
+            self.end_point = f"https://{api_id}.execute-api.us-east-1.amazonaws.com/Prod/register_taxi/"
 
 
     def create_registration(self, email, first_name, last_name, taxi_class):
@@ -33,29 +43,35 @@ class RegistrationSimulator:
         with open(f".certs/{taxi_id}.private.key", "w+") as key_file:
             key_file.write(data.get("priv_key"))
 
-def register_async():
+def register_async(taxi):
     registration_sim = RegistrationSimulator()
-    data = requests.get("https://randomuser.me/api/").json()
-    email = data.get("results")[0].get("email")
-    name = data.get("results")[0].get("name")
-    taxi_class = random.choice([0, 1, 2])
-    registration = registration_sim.create_registration(email, name["first"], name["last"], taxi_class)
+    registration = registration_sim.create_registration(**taxi)
     registration.register()
 
 def main():
     ntaxis = 0
+    sample_taxis = read_taxis("sample_taxis.json")
     while True:
-        ans = input("No of taxis to simulate: ")
+        ans = input("No of taxis to register: ")
         try:
             ntaxis = int(ans)
+            if ntaxis < 0:
+                print("Invalid entry, try again!.")
+                continue
+            if ntaxis == 0:
+                print("No taxis to register, exiting.")
+                import sys; sys.exit(0)
+            if ntaxis > len(sample_taxis):
+                print("Not enough sample data, try again!.")
+                continue
             break
         except:
             print("Invalid entry, try again!.")
 
-    print(f"Registering {ntaxis} taxis")
+    print(f"Registering taxis")
     tasks = []
-    for i in range(ntaxis):
-        register_async()
+    for taxi in sample_taxis[:ntaxis]:
+        register_async(taxi)
 
 
 if __name__ == '__main__':
