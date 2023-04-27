b2bd5ce42df180e8a78c8c4ab760949b0b720dfe
Sanjiv Singh <sk.sanjiv@gmail.com>
2023-04-27 16:35:04 +0530

    async handler for db insert and update

--- taxi-db/taxidb_lambda.py
+++ taxi-db/taxidb_lambda.py
@@ -2,6 +2,7 @@ import os
 import json
 import pymongo
 from bson.objectid import ObjectId
+import asyncio
 
 
 #Get Amazon DocumentDB ceredentials from environment variables
@@ -9,29 +10,32 @@ username = os.environ.get("db_user")
 password = os.environ.get("db_pass")
 endpoint = os.environ.get("db_endpoint")
 
-def lambda_handler(event, context):   
+#Connect to Amazon DocumentDB
+client = pymongo.MongoClient(endpoint, username=username, password=password,
+        tls='true', tlsCAFile='rds-combined-ca-bundle.pem', retryWrites='false')
+db = client.taxidb
+taxi_collection = db['taxi']
+taxi_history = db['taxi_history']
 
-    print("Received data: ")
-    print(event)
-    taxi_data = event
-
-    #Connect to Amazon DocumentDB
-    client = pymongo.MongoClient(endpoint, username=username, password=password,
-            tls='true', tlsCAFile='rds-combined-ca-bundle.pem', retryWrites='false')
-    db = client.taxidb
-    taxi_collection = db['taxi']
-    taxi_history = db['taxi_history']
-
-    #Insert data
-    result = taxi_history.insert_one(taxi_data)
-    print(f"Inserted into taxi history: {result.inserted_id}")
-
+async def update_taxi_collection(taxi_data):
     #Update data
     taxi_data['_id'] = ObjectId(taxi_data['taxi_id'])
     del taxi_data['taxi_id']
     result = taxi_collection.replace_one({"_id": taxi_data["_id"]}, taxi_data, upsert=True)
     print(f"Updated {result.matched_count} item in taxi collection: {result.upserted_id}")
 
-    #Close connection
-    client.close()
+async def insert_taxi_history(taxi_data):
+    #Insert data
+    result = taxi_history.insert_one(taxi_data)
+    print(f"Inserted into taxi history: {result.inserted_id}")
+
+
+def lambda_handler(event, context):   
+
+    print("Received data: ")
+    print(event)
+    taxi_data = event
+
+    asyncio.get_event_loop().run_until_complete(insert_taxi_history(taxi_data))
+    asyncio.get_event_loop().run_until_complete(update_taxi_collection(taxi_data))
 
