f98b4876c4aa7ff929b8763f1e66e8cfb4d64413
Sanjiv Singh <sk.sanjiv@gmail.com>
2023-05-09 01:18:17 +0530

    drop existing collections before creating

--- taxi-dev/db_setup.js
+++ taxi-dev/db_setup.js
@@ -1,24 +1,15 @@
 
 use taxidb;
 
-db.createCollection("taxi_registration");
+db.user.drop();
+db.taxi.drop();
+db.taxi_history.drop();
+
+db.createCollection("user");
 db.createCollection("taxi");
 db.createCollection("taxi_history");
-db.createCollection("user");
-db.createCollection("user_history");
 
+db.user.createIndex( { email: 1 }, { unique: true } )
 db.taxi.createIndex({location: "2dsphere"});
 db.taxi.createIndex( { email: 1 }, { unique: true } )
-db.user.createIndex( { email: 1 }, { unique: true } )
 
-// db.taxi.find( { 
-//     location: { 
-//        $near: { 
-//            $geometry: { 
-//                type: "Point", coordinates: [78.0521, 13.409] 
-//            }, 
-//            $minDistance: 0, 
-//            $maxDistance: 5000 
-//        } 
-//    } 
-//} );
