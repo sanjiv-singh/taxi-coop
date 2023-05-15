19802b532c0527a14c074b539cfd65ab945fcb64
Sanjiv Singh <sk.sanjiv@gmail.com>
2023-05-15 19:31:47 +0530

    refactored user sim to be oo

--- simulation/users_sim.py
+++ simulation/users_sim.py
@@ -37,53 +37,64 @@ def create_random_location(south_west, north_east):
     lng = random.uniform(south_west[1], north_east[1])
     return f"{lat},{lng}"
 
-async def request_ride(data):
-    response = requests.post(REQUEST_RIDE_END_POINT, json=data).json()
-    ride_id = response.get("ride_id")
-    print(f'Requested ride with id {ride_id}')
-    accepted_taxis = []
-    while True:
-        await asyncio.sleep(2)
-        url = f'{RIDES_END_POINT}/{ride_id}/'
-        try:
-            ride = requests.get(url).json()
-            accepted_taxis = ride[0].get("accepted_taxis")
-            if accepted_taxis:
-                break
-        except:
-            print("No taxis yet. Waiting..")
-    print(f'{len(accepted_taxis)} accepted the request')
-    # Select taxi that responded first
-    taxi_timings = [{"taxi_id": taxi["taxi_id"], "timing": parse_isodatetime(
-        taxi["timestamp"]["$date"])} for taxi in accepted_taxis]
-    sorted_taxis = sorted(taxi_timings, key=lambda x: x["timing"])
-    return sorted_taxis[0]["taxi_id"]
+class User:
+
+    def __init__(self, **user_data):
+        self.user_id = user_data.get("_id")
+        self.first_name = user_data.get("first_name")
+        self.last_name = user_data.get("last_name")
+        self.email = user_data.get("email")
 
 
-async def book_ride(user):
-    data = {
-        "user_id": user.get("_id"),
-        "taxi_class": random.choice([0, 1, 2, 3]),
-        "origin": create_random_location((12.8, 77.5), (13.5, 78.2)),
-        "destination": create_random_location((12.8, 77.5), (13.5, 78.2)),
-        "taxi_limit": 3,
-    }
-    taxi_id = await request_ride(data)
-    if taxi_id is None:
-        return
-    data["taxi_id"] = taxi_id
-    print(f'Taxi {taxi_id} responded first')
-    response = requests.post(BOOK_RIDE_END_POINT,
-                             json=data).json()
-    if response.get("message") == 'updated succesfully':
-        print(f'Taxi {taxi_id} booked')
-    return None
+    async def request_ride(self):
+        response = requests.post(REQUEST_RIDE_END_POINT, json=self.data).json()
+        self.ride_id = response.get("ride_id")
+        print(f'Requested ride with id {self.ride_id}')
+        accepted_taxis = []
+        while True:
+            await asyncio.sleep(2)
+            url = f'{RIDES_END_POINT}/{self.ride_id}/'
+            try:
+                ride = requests.get(url).json()
+                accepted_taxis = ride.get("accepted_taxis")
+                if accepted_taxis:
+                    break
+            except:
+                print("No taxis yet. Waiting..")
+        print(f'{len(accepted_taxis)} accepted the request')
+        # Select taxi that responded first
+        taxi_timings = [{"taxi_id": taxi["taxi_id"], "timing": parse_isodatetime(
+            taxi["timestamp"]["$date"])} for taxi in accepted_taxis]
+        sorted_taxis = sorted(taxi_timings, key=lambda x: x["timing"])
+        return sorted_taxis[0]["taxi_id"]
+
+
+    async def book_ride(self):
+        self.data = {}
+        self.data['user_id'] = self.user_id
+        self.data['taxi_class'] = random.choice([0, 1, 2, 3])
+        self.data['origin'] = create_random_location((12.8, 77.5), (13.5, 78.2))
+        self.data['destination'] = create_random_location((12.8, 77.5), (13.5, 78.2))
+        self.data['taxi_limit'] = 3
+
+        self.taxi_id = await self.request_ride()
+        if self.taxi_id is None:
+            return
+        self.data["taxi_id"] = self.taxi_id
+        print(f'Taxi {self.taxi_id} responded first')
+        response = requests.post(BOOK_RIDE_END_POINT,
+                                json=self.data).json()
+        if response.get("message") == 'updated succesfully':
+            print(f'Taxi {self.taxi_id} booked')
+        return None
 
 async def main(users):
 
     tasks = []
     for user in users:
-        tasks.append(asyncio.create_task(book_ride(user)))
+        user_obj = User(**user)
+        #tasks.append(asyncio.create_task(book_ride(user)))
+        tasks.append(asyncio.create_task(user_obj.book_ride()))
     await asyncio.gather(*tasks)
 
 if __name__ == '__main__':
